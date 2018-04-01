0ef1ee550fe1d29f2df9af5c6b784844fa3ce45f
Clément BOISSARD <clement.boissard@gmail.com>
2018-04-01 21:15:42 +0200

    Ajout des commentaires de la classe Livre

--- src/model/Livre.java
+++ src/model/Livre.java
@@ -32,48 +32,92 @@ public class Livre {
     public void setDataManager(DataManager dataManager) { this.dataManager = dataManager; }
     public DataManager getDataManager() { return dataManager; }
     
+    /**
+     * Constructeur par défaut de la classe Livre
+     * Initialise une liste de recettes vide
+     */
     public Livre(){
         setRecettes(new ArrayList<IRecette>());
     }
     
-    public Livre(DataManager mgr){
+    /**
+     * Constructeur secondaire à favoriser
+     * @param dataManager permettant la persistance
+     */
+    public Livre(DataManager dataManager){
         this();
-        setDataManager(mgr);
+        setDataManager(dataManager);
     }
     
+    /**
+     * Récupère une recette
+     * @param nom à partir du nom de la recette
+     * @return IRecette retourne une recette
+     */
     public IRecette getRecette(String nom){
         return getRecettes().stream().filter(r->r.getNom().equals(nom)).findFirst().get();
     }
     
+    /**
+     * Récupère l'index d'une recette dans la liste des recettes
+     * @param nom à partir du nom de la recette
+     * @return int l'index
+     */
     public int getRecetteIndex(String nom){
         return getRecettesObservable().indexOf(new Recette(nom, ""));
     }
     
+    /**
+     * Ajoute une recette dans la liste
+     * @param recette la recette à ajouter
+     */
     public void ajouterRecette(IRecette recette){
         if(!getRecettesObservable().contains(recette))
             getRecettesObservable().add(recette);
     }
     
+    /**
+     * Ajoute une liste de recette à la liste
+     * @param recettes la liste des recettes
+     */
     public void ajouterRecettes(List<IRecette> recettes){
         //setRecettes(recettes);
         for(IRecette recette : recettes)
             ajouterRecette(recette);
     }
     
+    /**
+     * Supprime une recette de la liste
+     * @param recette la recette à supprimer
+     */
     public void supprimerRecette(IRecette recette){
         getRecettes().remove(recette);
     }
     
+    /**
+     * Supprime une recette de la liste
+     * @param index la position de la recette à supprimer dans la liste
+     */
     public void supprimerRecette(int index){
         supprimerRecette(getRecettes().get(index));
     }
     
+    /**
+     * Modifie une recette
+     * @param nomEx le nom de la recette à modifier
+     * @param recette la recette contenant ses nouvelles valeurs
+     * Remplace la recette par la nouvelle
+     */
     public void modifierRecette(String nomEx, IRecette recette){
         int indexEx = getRecetteIndex(nomEx);
         getRecettesObservable().remove(indexEx);
         getRecettesObservable().add(indexEx, recette);
     }
     
+    /**
+     * Charge les recettes grâce à la persistance dans la liste
+     * @return boolean correspondant à la valeur de réussite de l'opération
+     */
     public boolean chargerRecettes(){
         if(dataManager == null) return false;
         getRecettesObservable().clear();
@@ -81,12 +125,20 @@ public class Livre {
         return true;
     }
     
+    /**
+     * Sauvegarde la liste des recettes grâce à la persistance
+     * @return boolean correspondant à la valeur de réussite de l'opération
+     */
     public boolean sauvegarderRecettes(){
         if(dataManager == null) return false;
         getDataManager().sauvegardeRecettes(getRecettes());
         return true;
     }
     
+    /**
+     * Redéfinition de la fonction toString
+     * @return String correspondant au texte à afficher
+     */
     public String toString(){
         String retour = "Livre de recettes :\n===================\n";
         if(getRecettes().size() < 1)
